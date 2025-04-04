b3c815d9cdf2eed7ed8170710db7232555f83da2
DeepakChaudhari <deepakchaudhari0418@gmail.com>
2025-04-04 17:06:29 +0530

    add some fix

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -3,6 +3,7 @@ import User from "../models/User";
 
 //API controller function to manage clerk user with database
 
+
 export const clerkWebhooks = async (req, res) =>{
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-import express from 'express'
+// import express from 'express'
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema(
@@ -16,6 +16,6 @@ const userSchema = new mongoose.Schema(
     }, {timestamps: true}
 );
 
-const User = mongoose.model ('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User
