2b3263d75381c899f4aa9f812b3da85a663fda6d
Deepak Chaudhari <deepakchaudhari0418@gmail.com>
2025-04-20 15:58:43 +0530

    backend complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,7 @@ export const purchaseCourse = async (req, res) => {
         const courseData = await Course.findById(courseId)
 
         if (!userData || !courseData) {
-            return res.json({ success: false, message: 'Data Not found', reason: `${userData || courseData}`, courseDetail: courseData})
+            return res.json({ success: false, message: 'Data Not found', reason: `${userData || courseData}`, courseDetail: courseData })
         }
 
         const purchaseData = {
@@ -121,8 +121,50 @@ export const updateUserCourseProgress = async (req, res) => {
 //Get user Course Progress
 export const getUserCourseProgress = async (req, res) => {
     try {
-
+        const userId = req.auth.userId
+        const { courseId } = req.body
+        const progressData = await CourseProgress.findOne({ userId, courseId })
+        res.json({ success: true, progressData })
     } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
 
+//Add User Rating to course
+
+export const addUserRating = async (req, res) => {
+    const userId = req.auth.userId;
+    const { courseId, rating } = req.body;
+
+    if (!courseId || !userId || !rating || rating < 1 || rating > 5) {
+        return res.json({ success: false, message: 'Invalid Details' });
+    }
+
+    try {
+        const course = await Course.findById(courseId);
+
+        if (!course) {
+            return res.json({ success: false, message: 'Course not found' });
+        }
+
+        const user = await User.findById(userId);
+
+        if (!user || !user.enrolledCourses.includes(courseId)) {
+            return res.json({ success: false, message: 'User has not purchased this course.' });
+        }
+
+        const existingRatingIndex = course.courseRatings.findIndex(r => r.userId === userId)
+
+        if (existingRatingIndex > -1) {
+            course.courseRatings[existingRatingIndex].rating = rating;
+        } else {
+            course.courseRatings.push({ userId, rating })
+        }
+
+        await course.save();
+
+        return res.json({ success: true, message: 'Rating added' })
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
     }
 }
